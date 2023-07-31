95ba01e63453358839561614baed1e002fc1ea24
joukey123 <0162943362@naver.com>
2023-07-31 19:46:13 +0900

    wetube_copy
    wetube_copy_

---
+++ src/controllers/videoConrtoller.js
@@ -0,0 +1,116 @@
+import Video from "../models/Video.js";
+import User from "../models/User.js";
+
+export const home = async (req, res) => {
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id).populate("owner");
+  console.log(video);
+
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
+};
+
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById({ _id: id });
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
+  console.log(video.owner);
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("Upload", { pageTitle: `Upload Video` });
+};
+
+export const postUpload = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
+  const { path: fileUrl } = req.file;
+  const { title, description, hashtags } = req.body;
+  try {
+    const newVideo = await Video.create({
+      title: title,
+      description: description,
+      fileUrl,
+      owner: _id,
+      hashtags: Video.formatHashtags(hashtags),
+    });
+    const user = await User.findById(_id);
+    user.videos.push(newVideo._id);
+    user.save();
+    return res.redirect(`/`);
+  } catch (error) {
+    console.log(error._message);
+    return res.status(400).render("Upload", {
+      pageTitle: `Upload Video`,
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getDelete = async (req, res) => {
+  const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id);
+  const user = await User.findById(_id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
+  await Video.findByIdAndDelete(id);
+  user.videos.splice(user.videos.indexOf(id), 1);
+  user.save();
+  return res.redirect(`/`);
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: { $regex: new RegExp(keyword, "i") },
+    });
+  }
+  return res.render("search", { pageTitle: "search", videos });
+};
---
+++ src/routers/userRouter.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  postEdit,
+  getEdit,
+  getChangePassword,
+  postChangePassword,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
+import {
+  protectorMiddleware,
+  publicOnMiddleware,
+  avatarUpload,
+} from "../middlewares";
+
+const userRouter = express.Router();
+
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
+userRouter.get("/github/start", publicOnMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnMiddleware, finishGithubLogin);
+userRouter.get("/:id", see);
+
+export default userRouter;
